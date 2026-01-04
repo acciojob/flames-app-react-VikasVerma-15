9746720e32329c096aeb663a3df47b5fe1e4490a
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2026-01-04 18:07:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,31 +11,32 @@ class App extends Component {
     };
   }
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   };
 
   calculateRelationship = () => {
     const { name1, name2 } = this.state;
 
-    if (!name1 || !name2) {
+    if (name1 === "" || name2 === "") {
       this.setState({ result: "Please Enter valid input" });
       return;
     }
 
-    let arr1 = name1.split("");
-    let arr2 = name2.split("");
+    let a = name1.split("");
+    let b = name2.split("");
 
-    // Remove common characters (case-sensitive)
-    for (let i = 0; i < arr1.length; i++) {
-      let index = arr2.indexOf(arr1[i]);
+    for (let i = 0; i < a.length; i++) {
+      let index = b.indexOf(a[i]);
       if (index !== -1) {
-        arr1[i] = "";
-        arr2[index] = "";
+        a[i] = "";
+        b[index] = "";
       }
     }
 
-    const count = arr1.join("").length + arr2.join("").length;
+    const count = a.join("").length + b.join("").length;
     const mod = count % 6;
 
     const flames = {
@@ -61,37 +62,33 @@ class App extends Component {
   render() {
     return (
       <div id="main">
-        {/* Do not remove the main div */}
-
         <input
           type="text"
-          data-testid="input1"
           name="name1"
+          data-testid="input1"
           value={this.state.name1}
           onChange={this.handleChange}
-          placeholder="Enter first name"
         />
 
         <input
           type="text"
-          data-testid="input2"
           name="name2"
+          data-testid="input2"
           value={this.state.name2}
           onChange={this.handleChange}
-          placeholder="Enter second name"
         />
 
         <button
-          data-testid="calculate_relationship"
           name="calculate_relationship"
+          data-testid="calculate_relationship"
           onClick={this.calculateRelationship}
         >
           Calculate Relationship Future
         </button>
 
         <button
-          data-testid="clear"
           name="clear"
+          data-testid="clear"
           onClick={this.clearAll}
         >
           Clear
@@ -104,3 +101,4 @@ class App extends Component {
 }
 
 export default App;
+
